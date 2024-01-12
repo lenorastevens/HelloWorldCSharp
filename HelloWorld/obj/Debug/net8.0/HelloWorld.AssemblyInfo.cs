bf856342ffe4618d834a671e77f6eec7d36e4ed8
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-01-11 20:19:22 -0600

    ran from different terminal

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca0900cbc7777f485c37cd7600219e8e1bb4a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
